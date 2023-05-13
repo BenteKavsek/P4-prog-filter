26b9be5e968206b28e0823e720de3c2c4148ee5f
BenteKavsek <35726@ma-web.nl>
2023-05-13 22:48:36 +0200

    results na video 8/11

--- js/main.js
+++ js/main.js
@@ -1,15 +1,56 @@
 let allGames = document.getElementsByClassName("game");
-
-for (let i = 0; i < allGames.length; i++){
-    
-}
-
 let filters = document.getElementsByClassName("filter");
-filters[0].onchange = function(){
-    if(filter[0].checked === true){
 
+let gtaFilter = document.getElementById("checkbox-gta");
+gtaFilter.onchange = function(){
+    if(gtaFilter.checked === true){
+        for (let i = 0; i < allGames.length; i++){
+            if (allGames[i].dataset.category === "GTA"){
+                allGames[i].style.display = "block";
+            }
+        }
     }
     else{
-        
+        for (let i = 0; i < allGames.length; i++){
+            if (allGames[i].dataset.category === "GTA"){
+                allGames[i].style.display = "none";
+            }
+        }
+    }
+}
+
+let hogwardsFilter = document.getElementById("checkbox-hogwards");
+hogwardsFilter.onchange = function(){
+    if(hogwardsFilter.checked === true){
+        for (let i = 0; i < allGames.length; i++){
+            if (allGames[i].dataset.category === "HP"){
+                allGames[i].style.display = "block";
+            }
+        }
+    }
+    else{
+        for (let i = 0; i < allGames.length; i++){
+            if (allGames[i].dataset.category === "HP"){
+                allGames[i].style.display = "none";
+            }
+        }
+    }
+}
+
+let minecraftFilter = document.getElementById("checkbox-minecraft");
+minecraftFilter.onchange = function(){
+    if(minecraftFilter.checked === true){
+        for (let i = 0; i < allGames.length; i++){
+            if (allGames[i].dataset.category === "MC"){
+                allGames[i].style.display = "block";
+            }
+        }
+    }
+    else{
+        for (let i = 0; i < allGames.length; i++){
+            if (allGames[i].dataset.category === "MC"){
+                allGames[i].style.display = "none";
+            }
+        }
     }
 }

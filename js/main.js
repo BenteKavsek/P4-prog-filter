4b597d2ff0d3c0c8f77b140de1dfe5f507a78f7b
BenteKavsek <35726@ma-web.nl>
2023-05-13 22:32:15 +0200

    results na video 7/11

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let allGames = document.getElementsByClassName("game");
+
+for (let i = 0; i < allGames.length; i++){
+    
+}
+
+let filters = document.getElementsByClassName("filter");
+filters[0].onchange = function(){
+    if(filter[0].checked === true){
+
+    }
+    else{
+        
+    }
+}
